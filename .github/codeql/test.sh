9247ac0ebf23c66fab5971b4b8925279e6cb2630
Arshia Moghimi <arshiamoghimi98@gmail.com>
2023-02-28 11:30:31 -0800

    added manual codeql workflow

---
+++ .github/codeql/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt update
+sudo apt install -y unzip
+cd ../../high-level-flow/code
+queries=(file requests socket boto3)
+mkdir -p query_results
+
+wget https://github.com/github/codeql-cli-binaries/releases/download/v2.12.3/codeql-linux64.zip
+unzip codeql-linux64.zip
+export PATH=$PATH:$(pwd)/codeql
+
+codeql pack install
+codeql database create codeqldb --language=python --overwrite
+for query in "${queries[@]}"; do
+    codeql database analyze codeqldb --format=csv --output=query_results/$query.csv queries/$query.ql
+done
+
+cat query_results/*.csv
