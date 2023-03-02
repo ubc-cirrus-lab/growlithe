0ceb6deb31cb8b9fbe810134ec298944c89a01ce
Arshia Moghimi <arshiamoghimi98@gmail.com>
2023-03-02 09:36:57 -0800

    fix: codeql not analyzing boto3 and requests libraries

--- .github/codeql/run.sh
+++ .github/codeql/run.sh
@@ -6,7 +6,6 @@ queries=(file requests socket boto3)
 
 cd ./high-level-flow
 
-sudo apt install python3-pip
 wget https://github.com/github/codeql-cli-binaries/releases/download/v2.12.3/codeql-linux64.zip
 unzip codeql-linux64.zip
 export PATH=$PATH:$(pwd)/codeql
@@ -15,7 +14,7 @@ cd ./code
 mkdir -p query_results
 
 codeql pack install
-pip3 install -r requirements.txt
+python -m pip install -r requirements.txt
 
 codeql database create codeqldb --language=python --overwrite
 for query in "${queries[@]}"; do
