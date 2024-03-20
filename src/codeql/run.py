55f32950d6ab18739335b9c716296139f3d46a3c
Arshia Moghimi <arshiamoghimi98@gmail.com>
2024-03-20 11:42:36 -0700

    feat: added trigger taint tracking (#56)

--- src/codeql/run.py
+++ src/codeql/run.py
@@ -1,14 +1,18 @@
 from codeql import CodeQL
 import os
+
+from src import benchmark_config
 from src.logger import logger
+
 # from src.config import app_path, rerun_db_create, rerun_queries
 
-queries = ['dataflows']
+queries = ["dataflows"]
 num_runs = 1
-app_path = "D:\\Code\\growlithe-results\\Benchmark-1\\moderated-image-processing\\"
+# app_path = "D:\\Code\\growlithe-results\\Benchmark-1\\moderated-image-processing\\"
+app_path = benchmark_config.app_path
 # app_path = "D:\\Code\\growlithe-results\\Benchmark-2\\claim-processing\\"
 rerun_db_create = True
 rerun_queries = True
 
-logger.info(f'Running {num_runs} CodeQL analyses on {app_path}')
+logger.info(f"Running {num_runs} CodeQL analyses on {app_path}")
 CodeQL.analyze(app_path, queries, rerun_db_create, rerun_queries, num_runs=num_runs)
