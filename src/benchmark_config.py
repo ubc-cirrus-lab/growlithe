3f57c99844d0e748e2560cc3a946d0d92b1f9595
Praveen Gupta <pvgupta24@gmail.com>
2024-03-22 13:20:54 -0700

    Updates logger to send profiling logs to file

--- src/benchmark_config.py
+++ src/benchmark_config.py
@@ -16,19 +16,20 @@ elif system_platform == 'Darwin':
 ##### App Config
 
 # App Path
+# app_path =  os.path.join(growlithe_results_path, "Benchmark-1", "moderated-image-processing")
 app_path =  os.path.join(growlithe_results_path, "Benchmark-2", "claim-processing")
 # Directory to store updated app, and intermediate growlithe files like codeql db, edge policies, etc.
 app_growlithe_path = os.path.join(app_path, "..", "growlithe")
-
+app_growlithe_output_path = os.path.join(app_growlithe_path, "output")
+profiler_log_path = os.path.join(app_growlithe_output_path, "growlithe_profiler.log")
 
 ##### Codeql Config
 codeql_db_path = os.path.join(app_growlithe_path, "codeqldb")
-edge_policy_path = os.path.join(app_growlithe_path, "output", "edge_policies.json")
-
 num_runs = 1
-rerun_db_create = False
+rerun_db_create = True
 rerun_queries = True
 queries_to_run = ["dataflows"]
 
 # Growlithe Policy Config
 regenerate_edge_policy = False
+edge_policy_path = os.path.join(app_growlithe_output_path, "edge_policies.json")
--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,42 @@
 import logging.config
+from src.benchmark_config import *
 
-logging.config.dictConfig({"version": 1, "disable_existing_loggers": True})
-logging.basicConfig(
-    format="[%(asctime)s] [%(levelname)s] %(message)s", level=logging.INFO
-)
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console_formatter": {
+            "format": "[%(asctime)s] [%(levelname)s] %(message)s"
+        },
+        "file_formatter": {
+            "format": "[%(asctime)s] [%(levelname)s] %(message)s"
+        }
+    },
+    "handlers": {
+        "console_handler": {
+            "class": "logging.StreamHandler",
+            "formatter": "console_formatter",
+            "level": "INFO"
+        },
+        "file_handler": {
+            "class": "logging.FileHandler",
+            "formatter": "file_formatter",
+            "filename": profiler_log_path,
+            "level": "INFO"
+        }
+    },
+    "loggers": {
+        "main": {
+            "handlers": ["console_handler"],
+            "level": "INFO"
+        },
+        "profiler": {
+            "handlers": ["file_handler"],
+            "level": "INFO"
+        }
+    }
+})
+
+# Get the loggers
 logger = logging.getLogger("main")
+profiler_logger = logging.getLogger("profiler")
