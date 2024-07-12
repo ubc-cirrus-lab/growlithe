88e681ca6462c4c4705fb500a1722a3fb490bc1c
Praveen Gupta <pvgupta24@gmail.com>
2024-07-12 14:38:09 -0700

    Adds support for parsing basic policies

--- enforcer/policy/growlithe.py
+++ enforcer/policy/growlithe.py
@@ -2,35 +2,69 @@ from pyDatalog import pyDatalog
 from collections import defaultdict
 import os
 
-default_value = lambda: {os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+default_value = lambda: {os.environ["AWS_LAMBDA_FUNCTION_NAME"]}
 GROWLITHE_TAINTS = defaultdict(default_value)
 
+##==================================================================#
+# Arithemtic predicates for numerical operations
+pyDatalog.load(
+    """
+    add(X, Y, Z) <= (X == Y + Z)
+    sub(X, Y, Z) <= (X == Y - Z)
+               
+    mul(X, Y, Z) <= (X == Y * Z)
+    div(X, Y, Z) <= (X == Y / Z)
+"""
+)
 
-pyDatalog.load("""
+# Comparison predicates for numerical operations
+pyDatalog.load(
+    """
     eq(X, Y) <= (X == Y)
-    gt(X, Y) <= (X > Y)
     lt(X, Y) <= (X < Y)
-""")
+    le(X, Y) <= (X <= Y)
+    gt(X, Y) <= (X > Y)
+    ge(X, Y) <= (X >= Y)
 """
-String predicates
-"""
-# @pyDatalog.predicate()
-# def isPrefix(x, y):
-#     if x.id.startswith(y.id):
-#         yield (x, y)
+)
+
 
 @pyDatalog.predicate()
-def isSuffix(x, y):
-    if x.id.endswith(y.id):
-        yield (x, y)
+def not_2(X, Y):
+    if X.is_const():
+        yield (X.id, not (X.id))
+    elif Y.is_const():
+        yield (not (Y.id), Y.id)
 
+
+##==================================================================#
+# String predicates
+@pyDatalog.predicate()
+def isPrefix2(str, pre):
+    if str.id.startswith(pre.id):
+        yield (str, pre)
+
+
+@pyDatalog.predicate()
+def isSuffix2(str, suf):
+    if str.id.endswith(suf.id):
+        yield (str, suf)
+
+
+@pyDatalog.predicate()
+def hasSubstr2(str, substr):
+    if str.id.startswith(substr.id):
+        yield (str, substr)
+
+
+##==================================================================#
 # """Taint predicates"""
 @pyDatalog.predicate()
-def taintSetContains(node_id, label):
+def taintSetIncludes(node_id, label):
     global GROWLITHE_TAINTS
-    if node_id.id in GROWLITHE_TAINTS:
-        if label.id in GROWLITHE_TAINTS[node_id.id]:
-            yield True
+    if node_id.id in GROWLITHE_TAINTS and label.id in GROWLITHE_TAINTS[node_id.id]:
+        yield True
+
 
 @pyDatalog.predicate()
 def taintSetExcludes(node_id, label):
@@ -39,61 +73,81 @@ def taintSetExcludes(node_id, label):
         if label.id in GROWLITHE_TAINTS[node_id.id]:
             yield False
         yield True
+    # FIXME: Decide what to do if node id not in taint set, so cannot check label
 
+
+##==================================================================#
 # Session properties are retrieved at runtime
 # Retrieved values are set for the required datalog variable
 # in the policy assertion at runtime
-def getSessionProp(prop):
+def getSessionVar(prop):
     if prop == "SessionTime":
         import time
+
         return round(time.time())
     elif prop == "SessionRegion":
         import os
-        return os.environ['AWS_REGION']
+
+        return os.environ["AWS_REGION"]
+
 
 def getMetaProp(prop, resource_type, resource_name):
     if prop == "MetaConduitRegion":
         if resource_type == "S3_BUCKET":
             import boto3
-            client = boto3.client('s3')
-            return client.get_bucket_location(Bucket=resource_name)['LocationConstraint']
+
+            client = boto3.client("s3")
+            return client.get_bucket_location(Bucket=resource_name)[
+                "LocationConstraint"
+            ]
         elif resource_type == "DYNAMODB_TABLE":
             import boto3
-            client = boto3.client('dynamodb')
-            return client.describe_table(TableName=resource_name)['Table']['TableArn'].split(':')[3]
+
+            client = boto3.client("dynamodb")
+            return client.describe_table(TableName=resource_name)["Table"][
+                "TableArn"
+            ].split(":")[3]
 
     elif prop == "MetaConduitResourceName":
         return resource_name
-    
+
+
 @pyDatalog.predicate()
 def getItemVal5(val, table_name, key_name, key, prop):
-    print('Getting Value')
+    print("Getting Value")
     import boto3
-    dynamodb = boto3.resource('dynamodb')
+
+    dynamodb = boto3.resource("dynamodb")
     print(table_name.id, key_name.id, key.id, prop.id)
     table = dynamodb.Table(table_name.id)
     print(table)
-    response = table.get_item(Key={f"{key_name.id}": key.id})['Item']
+    response = table.get_item(Key={f"{key_name.id}": key.id})["Item"]
     print(response)
     yield (response[prop.id], table_name, key_name, key, prop)
+
 
 @pyDatalog.predicate()
 def ipToCountry(ip, out):
     import urllib.request
     import json
 
-    endpoint = f'https://ipinfo.io/{ip.id}/json'
+    endpoint = f"https://ipinfo.io/{ip.id}/json"
 
     try:
         with urllib.request.urlopen(endpoint) as response:
             data = json.load(response)
-            yield (ip, data['country'])
+            yield (ip, data["country"])
     except urllib.error.URLError:
         pass
-    
+
+
 def getUserAttribute(event, attr):
     import json
-    return json.loads(event['requestContext']['authorizer']['claims'][attr])['formatted']
+
+    return json.loads(event["requestContext"]["authorizer"]["claims"][attr])[
+        "formatted"
+    ]
+
 
 def getDictNestedKeyVal(dictionary, nestedKeys):
     inner = dictionary
---
+++ enforcer/policy/policy.py
@@ -0,0 +1,143 @@
+import re
+from typing import List, Set
+
+
+class PolicyPredicate:
+    """AND separated policy predicate in a DNF policy clause"""
+
+    def __init__(self, predicate_str: str):
+        self.predicate_str = predicate_str.strip()
+        self.predicate_name, self.arguments = self.parse_predicate()
+        # Datalog variables used in the predicate
+        self.variables = self.extract_variables()
+        self.predicate_name
+
+    def parse_predicate(self):
+        match = re.match(r"(\w+)\s*\((.*)\)$", self.predicate_str)
+        if match:
+            name = match.group(1)
+            args = [arg.strip() for arg in match.group(2).split(",")]
+            return name, args
+        else:
+            print(f"Error: '{self.predicate_str}' is not a valid function predicate")
+            return "", []
+
+    def extract_variables(self) -> Set[str]:
+        variables = set()
+        for arg in self.arguments:
+            # If the argument is not a string literal or a number, consider it a variable
+            if not (arg.startswith(("'", '"')) or arg.replace(".", "").isdigit()):
+                variables.add(arg)
+        return variables
+
+
+class PredicateSet:
+    def __init__(self, predicates: Set[PolicyPredicate]):
+        self.predicates = predicates
+        self.variables = self.extract_variables()
+
+    def extract_variables(self) -> Set[str]:
+        variables = set()
+        for pred in self.predicates:
+            variables.update(pred.variables)
+        return variables
+
+    @property
+    def contains_session_variables(self) -> bool:
+        return any(var.startswith("Session") for var in self.variables)
+
+
+class PolicyClause:
+    """OR separated policy clause in a DNF policy"""
+
+    def __init__(self, predicates: List[PolicyPredicate]):
+        self.predicates = predicates
+        self.disjoint_predicates = self.divide_into_disjoint_sets()
+
+    # # Each clause can have set of predicates that can be evaluated independently of the other predicates
+    def divide_into_disjoint_sets(self) -> List[List[PolicyPredicate]]:
+        disjoint_sets = []
+        predicates = self.predicates.copy()
+        while predicates:
+            predicate = predicates.pop(0)
+            current_set = [predicate]
+            current_vars = predicate.variables
+            i = 0
+            while i < len(predicates):
+                if current_vars & predicates[i].variables:
+                    current_set.append(predicates.pop(i))
+                    current_vars.update(current_set[-1].variables)
+                else:
+                    i += 1
+            disjoint_sets.append(current_set)
+        return disjoint_sets
+
+    def insert_implicit_predicate(self):
+        for disjoint_set in self.disjoint_predicates:
+            variables: Set[str] = set()
+
+            # Collect all datalog variables used in the set of predicates
+            for pred in disjoint_set:
+                variables.update(pred.variables)
+
+            # TODO: Replace with properties stored in node/edge objects
+            for var in variables:
+                if var.startswith("Session"):
+                    disjoint_set.append(
+                        PolicyPredicate(f"eq({var}, '{{getSessionVar('{var}')}}')")
+                    )
+                elif var.startswith("Inst"):
+                    raise NotImplementedError("Instance variables not supported yet")
+                elif var.startswith("Object"):
+                    raise NotImplementedError("Object variables not supported yet")
+                else:
+                    pass
+
+    @property
+    def query(self) -> str:
+        return " & ".join([f"{pred.predicate_str}" for pred in self.predicates])
+
+    def insert_implicit_predicate(self):
+        for pred in self.predicates:
+            pass
+
+
+class Policy:
+    def __init__(self, policy_type: str, policy_str: str):
+        self.policy_type = policy_type
+        self.policy_str = policy_str
+        self.policy_clauses: List[PolicyClause] = self.parse_policy_str(policy_str)
+
+    def parse_policy_str(self, policy_str: str) -> List[PolicyClause]:
+        # Split the policy string into clauses (separated by 'or')
+        clause_strs = re.split(r"\s+or\s+", policy_str.strip())
+
+        clauses: List[PolicyClause] = []
+        for clause_str in clause_strs:
+            # Remove outer parentheses
+            clause_str = clause_str.strip()[1:-1]
+
+            # Split the clause into predicates (separated by '&')
+            predicate_strs = re.split(r"\s*&\s*", clause_str)
+
+            predicates: List[PolicyPredicate] = [
+                PolicyPredicate(pred) for pred in predicate_strs
+            ]
+            clauses.append(PolicyClause(predicates))
+        return clauses
+
+    def generate_assertion(self, language) -> str:
+        if language == "python":
+            return self.generate_python_assertion()
+
+    def generate_python_assertion(self) -> str:
+        # Generate the pyDatalog assertion string for python
+
+        pyDatalog_queries = [
+            f'pyDatalog.ask(f"{clause.query}") != None'
+            for clause in self.policy_clauses
+        ]
+        # Get the python statement: assert pyDatalog.ask(c1)!=None or pyDatalog.ask(c2)!=None or ... , "Policy evaluated to be false"
+        return (
+            f"assert {' or '.join(pyDatalog_queries)}, 'Policy evaluated to be false'"
+        )
