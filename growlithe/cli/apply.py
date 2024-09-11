1f9568d17c3b410d2cd62f0dc60381f54e86057b
Praveen Gupta <pvgupta24@gmail.com>
2024-09-10 18:01:17 -0700

    Minor bug fix and times analyze/apply

--- growlithe/cli/apply.py
+++ growlithe/cli/apply.py
@@ -3,13 +3,17 @@ import pickle
 from growlithe.graph.adg.graph import Graph
 from growlithe.common.file_utils import save_files
 from growlithe.enforcer.taint.taint_tracker import TaintTracker
+from growlithe.config import Config
+from growlithe.common.utils import profiler_decorator
 
-
-def apply(config):
+@profiler_decorator
+def apply(config: Config):
     with open(config.graph_dump_path, "rb") as f:
         graph: Graph = pickle.load(f)
+    print("0", config.pydatalog_layer_path)
     with open(config.config_dump_path, "rb") as f:
         app_config_parser = pickle.load(f)
+    print("1", config.pydatalog_layer_path)
 
     graph.get_updated_policy_json(config.policy_spec_path)
     # Enforcement
